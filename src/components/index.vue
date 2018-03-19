018a18780ebbb4026a64ab0028811b749d32e6a2
shizhier <shizhier@gmail.com>
2018-03-19 17:38:13 +0800

    第一次提交

---
+++ src/components/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>{{msg}}</div>
+</template>
+<script>
+export default {
+  name:"IndexComponent",
+  data:function(){
+      return {msg:"这是首页"}
+  }
+}
+</script>
+<style>
+
+</style>
+
